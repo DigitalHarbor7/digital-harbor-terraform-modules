13f341ab7a547697cc1cf858db232435e1023eb2
pypeaday <github@pype.aleeas.com>
2024-04-16 08:04:32 -0500

    make output dependent on zone

--- modules/terraform-digital-harbor-cloudflare/outputs.tf
+++ modules/terraform-digital-harbor-cloudflare/outputs.tf
@@ -1,5 +1,6 @@
 output "cloudflare_zone_name_servers" {
-  value = cloudflare_zone.zone.name_servers
+  value = length(cloudflare_zone.zone) > 0 ? cloudflare_zone.zone.name_servers : null
+  # value = cloudflare_zone.zone.name_servers
 }
 
 output "cloudflare_account_id" {
